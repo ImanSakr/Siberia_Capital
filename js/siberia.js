51adbb52530c493bc47f225cf2bba775c57146cb
ImanSakr <eng.emansakr@gmail.com>
2019-04-28 05:57:53 +0200

    First commit

---
+++ js/siberia.js
@@ -0,0 +1,39 @@
+
+// slide
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
+//end of slide
+
+// dismiss popup
+const top_left_button=document.getElementsByTagName('button')[0];
+const top_left_div=document.getElementsByClassName('top-left')[0];
+top_left_button.addEventListener('click',hide);
+
+function hide(){
+	top_left_div.style.display='none';
+}
